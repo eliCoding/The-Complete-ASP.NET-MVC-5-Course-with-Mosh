4f642c6166cda9ad10ab74fdf035c05f8b001f60
Your Name Here <e.amanollahi@gmail.com>
2018-07-04 09:39:58 -0700

    Initial Commit1

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+            return View(viewModel);
+
+        }
+
+        #region
+        public ActionResult Edit(int id)
+        {
+            return Content("id" + id);
+        }
+
+        #endregion
+
+
+        //movies
+        //string type in C# is reference type and it is nullable
+        #region
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
+        #endregion
+
+        #region
+        [Route("movies.released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        #endregion
+    }
+}
---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    // POCO : behave of our application
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    //    /movies/random
+}
